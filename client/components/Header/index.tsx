e7a30f7b0ff666e982aff3db30caf36d1e156e58
Aravin <4869265+Aravin@users.noreply.github.com>
2023-11-19 15:58:43 +0530

    Added tamil native logo

--- client/components/Header/index.tsx
+++ client/components/Header/index.tsx
@@ -57,14 +57,14 @@ const Header = () => {
                 } `}
               >
                 <Image
-                  src="/images/logo/logo-2.svg"
+                  src="/images/logo/tn-logo.jpeg"
                   alt="logo"
                   width={140}
                   height={30}
                   className="w-full dark:hidden"
                 />
                 <Image
-                  src="/images/logo/logo.svg"
+                  src="/images/logo/tn-logo-white.png"
                   alt="logo"
                   width={140}
                   height={30}
