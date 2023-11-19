882599503f9507500fc4b7628908a68f5a74c3ba
Aravin <4869265+Aravin@users.noreply.github.com>
2023-11-19 22:19:45 +0530

    Added sitemap

---
+++ client/app/sitemap.ts
@@ -0,0 +1,32 @@
+import { MetadataRoute } from 'next'
+
+const domain = 'https://www.tamilnative.com'
+ 
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: domain,
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 1,
+    },
+    {
+      url: domain + '/about',
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 0.8,
+    },
+    {
+      url: domain + '/contact',
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 0.5,
+    },
+    {
+        url: domain + '/terms',
+        lastModified: new Date(),
+        changeFrequency: 'weekly',
+        priority: 0.5,
+      },
+  ]
+}
