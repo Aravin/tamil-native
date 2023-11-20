c8147c6afbbab98c7d26e9b8eeff49363e4f999c
Aravin <4869265+Aravin@users.noreply.github.com>
2023-11-20 22:43:27 +0530

    Added zoho salesIQ

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -34,8 +34,13 @@ export default function RootLayout({
           gtag('config', '${GTAG_ID}');
         `}
         </Script>
-       
-
+        <Script id="zsiqchat">
+          {
+            `
+            var $zoho=$zoho || {};$zoho.salesiq = $zoho.salesiq || {widgetcode: "siq379cb93a9974a8bb78fe865c9364ea1dda81b8aca5f5840b446a9a3bed4407a0", values:{},ready:function(){}};var d=document;s=d.createElement("script");s.type="text/javascript";s.id="zsiqscript";s.defer=true;s.src="https://salesiq.zohopublic.in/widget";t=d.getElementsByTagName("script")[0];t.parentNode.insertBefore(s,t);
+            `
+          }
+        </Script>
       </head>
 
       <body className={`bg-[#FCFCFC] dark:bg-black ${inter.className}`}>
