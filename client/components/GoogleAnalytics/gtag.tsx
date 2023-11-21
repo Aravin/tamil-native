4cb573c0499dcd2113accc82f624f2e71025fa3c
Aravin <4869265+Aravin@users.noreply.github.com>
2023-11-21 23:15:03 +0530

    Added analytic event for each page change

--- client/components/GoogleAnalytics/gtag.tsx
+++ client/components/GoogleAnalytics/gtag.tsx
@@ -1,13 +1,13 @@
 export const GA_TRACKING_ID = process.env.NEXT_PUBLIC_GTAG;
 
 export const pageview = url => {
-  (window as any).gtag("config", GA_TRACKING_ID, {
+  window.gtag("config", GA_TRACKING_ID, {
     page_path: url,
   })
 }
 
 export const event = ({ action, category, label, value }) => {
-  (window as any).gtag("event", action, {
+  window.gtag("event", action, {
     event_category: category,
     event_label: label,
     value: value,
--- client/components/GoogleAnalytics/index.tsx
+++ client/components/GoogleAnalytics/index.tsx
@@ -1,19 +1,34 @@
+'use client';
+
 import Script from "next/script"
 import * as gtag from "./gtag"
 
+import { usePathname } from "next/navigation"
+import { useEffect } from "react";
+
 const GoogleAnalytics = () => {
+
+  const pathname = usePathname();
+
+  useEffect(() => {
+    const url = `${pathname}`;
+    console.log(url);
+    gtag.pageview(url);
+  }, [pathname]);
 
   return (
     <>
-      {process.env.NODE_ENV !== 'development' && <><Script
-        strategy="afterInteractive"
-        src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
-      />
-        <Script
-          id="gtag-init"
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `
+      {process.env.NODE_ENV === 'production' &&
+        <>
+          <Script
+            strategy="afterInteractive"
+            src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
+          />
+          <Script
+            id="gtag-init"
+            strategy="afterInteractive"
+            dangerouslySetInnerHTML={{
+              __html: `
                       window.dataLayer = window.dataLayer || [];
                       function gtag(){dataLayer.push(arguments);}
                       gtag('js', new Date());
@@ -21,8 +36,9 @@ const GoogleAnalytics = () => {
                       page_path: window.location.pathname,
                       });
                     `,
-          }}
-        /></>
+            }}
+          />
+        </>
       }
     </>
   )
